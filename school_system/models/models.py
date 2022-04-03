52331f9715ed4083fc5979b8a2ef1a9ff90bd9d3
May <msuwaiyel@mawhiba.org.sa>
2022-04-03 13:39:52 +0300

    add subject menu

--- school_system/models/models.py
+++ school_system/models/models.py
@@ -14,6 +14,7 @@ class Room(models.Model):
 class Subject(models.Model):
     _name = "school.subject"
     name = fields.Char("Name")
+    level=fields.Integer("Level", required=True)
 
 
 class Teachers(models.Model):
